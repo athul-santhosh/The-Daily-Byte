9ad1c72346c693c6384e7a3276d2a3f249f37d5f
Athul  Santhosh <42475189+thenex14@users.noreply.github.com>
2020-09-20 18:42:40 +0300

    Update Correct Capitalization.py

--- Correct Capitalization.py
+++ Correct Capitalization.py
@@ -3,10 +3,24 @@
 # "compUter", return false
 # "coding", return true
 
-
+# first approach
 def correctCapitil(s):
 	return s == s.lower() or s == s.upper() or s == s.title()
-
+# second approach
+def correctCapitil_(s):
+	first_capital = s[0].isupper()
+	capitals = 0
+	for i in s:
+		if i.isupper():
+			capitals += 1
+	if capitals == len(s):
+		return True 
+	elif capitals == 1 and first_capital is True:
+		return True
+	elif capitals == 0:
+		return True
+	else:
+		return False
 
 
 
